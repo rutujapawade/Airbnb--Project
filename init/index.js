c51e3d922eb248ef0158ee6c39682c2d272e2786
rutuja <pawaderutuja5@gmail.com>
2026-05-22 12:27:56 +0530

    fresh and clean push

---
+++ init/index.js
@@ -0,0 +1,61 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/Airbnb";
+const DB_URL = process.env.ATLASDB_URL;
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  // await mongoose.connect(MONGO_URL);
+  await mongoose.connect(DB_URL);
+}
+
+let categoryAll = [
+  "Beachfront",
+  "Cabins",
+  "Omg",
+  "Lake",
+  "Design",
+  "Amazing Pools",
+  "Farms",
+  "Amazing Views",
+  "Rooms",
+  "Lakefront",
+  "Tiny Homes",
+  "Countryside",
+  "Treehouse",
+  "Trending",
+  "Tropical",
+  "National Parks",
+  "Casties",
+  "Camping",
+  "Top Of The World",
+  "Luxe",
+  "Iconic Cities",
+  "Earth Homes",
+];
+
+//db me pehle data ho to vo delete ho and then new add
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "65fbc1dc8ee2ccda05f7194e",
+    category: [
+      `${categoryAll[Math.floor(Math.random() * 22)]}`,
+      `${categoryAll[Math.floor(Math.random() * 22)]}`,
+    ],
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+initDB();
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  fName: {
+    type: String,
+    required: true,
+  },
+  lName: {
+    type: String,
+    // required: true
+  },
+  image: {
+    url: String,
+    filename: String,
+  },
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
